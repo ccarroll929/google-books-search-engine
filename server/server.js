6443b6654c630fe939e64e795221d5f99ece0d2f
Christina Carroll <ccarroll0528@gmail.com>
2024-04-23 14:16:12 -0500

    imported authMiddleware and ApolloServer and created an ApolloServer object that uses our schemas and authMiddleware

--- server/server.js
+++ server/server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { authMiddleware } = require("./utils/auth");
+const { ApolloServer } = require("apollo-server-express");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+})
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +23,13 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app })
+}
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  app.listen(PORT, () => 
+  console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+  }
+);
