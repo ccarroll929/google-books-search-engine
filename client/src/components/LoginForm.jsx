0540d95771cb264677fe56f0d19aa4554ceeed97
Christina Carroll <ccarroll0528@gmail.com>
2024-04-23 17:16:54 -0500

    fixed typo so the login mutation is called correctly

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -39,7 +39,7 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await login(userFormData);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
