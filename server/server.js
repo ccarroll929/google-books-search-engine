591052754b2f813ff28639e30b1a83eab6a3995e
ccarroll929 <146889005+ccarroll929@users.noreply.github.com>
2024-04-26 11:58:31 -0500

    removed unnecessary dotenv import

--- server/server.js
+++ server/server.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const cors = require('cors');
 
 // Importing our authMiddleware from utils
 const { authMiddleware } = require("./utils/auth");
 
 // Importing Apollo server and schemas
-const { ApolloServer } = require("apollo-server-express");
+const { ApolloServer, gql } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Setting up GraphQL server using Apollo server
-const server = new ApolloServer({
+const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
+  introspection: true,
   context: authMiddleware,
 })
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -29,18 +31,17 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/'));
 })
 
-
 const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app })
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app, path: '/graphql' });
 }
 db.once('open', () => {
   app.listen(PORT, () => 
   console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+  console.log(`GraphQL at http://localhost:${PORT}${apolloServer.graphqlPath}`)
   }
 );
 
