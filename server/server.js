2427246e3a58acebc3a3728eca67afc593d67277
ccarroll929 <146889005+ccarroll929@users.noreply.github.com>
2024-04-27 14:21:55 -0500

    updated static paths from client/build to client/dist

--- server/server.js
+++ server/server.js
@@ -26,13 +26,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-// if we're in production, serve client/build as static assets
+// if we're in production, serve client/dist as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/'));
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 })
 
 const startApolloServer = async (typeDefs, resolvers) => {
