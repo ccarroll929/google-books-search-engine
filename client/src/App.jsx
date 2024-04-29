2fb325c33a1d2d839eefa60c35ec3afc116aa20f
ccarroll929 <146889005+ccarroll929@users.noreply.github.com>
2024-04-29 17:00:40 -0500

    changed PORT number, removed proxy and prod values from package.json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from "@apollo/client";
 import { setContext} from '@apollo/client/link/context';
 const httpLink = createHttpLink({
-  uri: "http://localhost:3001/graphql",
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers })=>{
